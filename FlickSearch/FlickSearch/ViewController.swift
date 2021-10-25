7620b63191195883c60f817d4f0a6138621ea6cf
binshakerr-7peaks <eslam.shaker@7peakssoftware.com>
2021-10-26 00:13:24 +0200

    project start

---
+++ FlickSearch/FlickSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlickSearch
+//
+//  Created by Eslam Shaker on 26/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red 
+    }
+
+
+}
+
